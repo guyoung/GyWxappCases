a379725e8bbab0399fede08476cce201bb9989b9
guyoung <guyoung@aliyun.com>
2016-12-31 23:54:43 +0800

    update

--- Enhanced/pages/generator/index.js
+++ Enhanced/pages/generator/index.js
@@ -1,6 +1,6 @@
 //index.js
 var Promise = require('../../lib/es6-promise/es6-promise').Promise;
-var co = require('../../lib/co/index');
+var co = require('../../lib/co/we-index');
 var regeneratorRuntime = require("../../lib/regenerator-runtime/runtime");
 
 //获取应用实例
@@ -10,24 +10,19 @@ Page({
 
 
     onReady: function () {
-        //console.log('onLoad');
+        //console.log('onReady');
         var that = this;
 
-        function co(gen) {
-            var it = gen();
-            var ret = it.next();
-            ret.value.then(function (res) {
-                it.next(res);
-            });
-        }
-        function sayhello() {
-            return Promise.resolve('hello').then(function (hello) {
-                console.log(hello);
-            });
-        }
-        co(function* helloworld() {
-            yield sayhello();
-            console.log('world');
-        });
+        var gen = function* () {
+            var a = Promise.resolve(1);
+            var b = Promise.resolve(2);
+            var c = Promise.resolve(3);
+            var res = yield [a, b, c];
+            console.log(res); // 输出：[1, 2, 3]
+        };
+
+        co(gen).then(function () {
+            console.log('Generator函数执行完毕了'); // 输出：Generator函数执行完毕了
+        });       
     }
 });
